fec88bee941acc33a0b77d15d696ea5d12087d65
HiggsInc <adrian@adrianhiggs.com>
2018-05-07 15:01:50 +0200

    Commands

---
+++ src/EssentialsBE/Commands/Economy/BalanceTop.php
@@ -0,0 +1,36 @@
+<?php
+namespace EssentialsBE\Commands\Economy;
+
+use EssentialsBE\BaseFiles\BaseAPI;
+use EssentialsBE\BaseFiles\BaseCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+class BalanceTop extends BaseCommand{
+    /**
+     * @param BaseAPI $api
+     */
+    public function __construct(BaseAPI $api){
+        parent::__construct($api, "balancetop", "See the top money", null, true, ["topbalance", "topmoney", "moneytop"]);
+        $this->setPermission("essentials.balancetop.use");
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param string $alias
+     * @param array $args
+     * @return bool
+     */
+    public function execute(CommandSender $sender, $alias, array $args): bool{
+        if(!$this->testPermission($sender)){
+            return false;
+        }
+        if(count($args) > 0){
+            $this->sendUsage($sender, $alias);
+            return false;
+        }
+        $sender->sendMessage(TextFormat::GREEN . " --- Money top list ---");
+        $this->getAPI()->sendBalanceTop($sender);
+        return true;
+    }
+}
